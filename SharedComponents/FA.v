c1026c84d7cdd97563697d87cea35c4600963714
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-02-21 12:30:55 -0500

    add completed, untested FA

--- SharedComponents/FA.v
+++ SharedComponents/FA.v
@@ -4,17 +4,16 @@ module FA (
     output Cout, Sum);
 
     // internal wires 
-    wire w1,w2,w3,w4,w5;
+    wire w1,w2,w3,w4,w5,w6,w7;
 
     nand U1 (w1, A, B);
     nand U2 (w2, A, w1);
-    nand U3 (w2, A, B);
-    nand U4 (w1, A, B);
-    nand U5 (w1, A, B);
-    nand U6 (w1, A, B);
-    nand U7 (w1, A, B);
-    nand U8 (w1, A, B);
-    nand U9 (w1, A, B);
-    
+    nand U3 (w3, w1, B);
+    nand U4 (w4, w2, w3);
+    nand U5 (w5, Cin, w4);
+    nand U6 (w6, w5, w4);
+    nand U7 (w7, Cin, w5);
+    nand U8 (Sum, w7, w6);
+    nand U9 (Cout, w1, w5);
     
 endmodule
